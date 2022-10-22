274b93d090076f0969c527eefa201e6eda9ae032
Le Thanh Trung <trung.le@navigosgroup.com>
2022-10-22 15:15:27 +0700

    update

--- src/reactapp/src/src/views/endpoint.js
+++ src/reactapp/src/src/views/endpoint.js
@@ -1,6 +1,6 @@
 // const endpoint =  "http://10.10.10.10"
 // const endpoint =  "http://localhost:8080"
 
-const endpoint =  ""
+const endpoint =  "https://api.trunglt.com"
 
 export default endpoint
