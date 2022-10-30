0dbbfda3bebccc7437cf73ec80623ff8608e44ba
Le Thanh Trung <trung.le@navigosgroup.com>
2022-10-30 08:55:31 +0700

    [fix] cors

--- src/reactapp/src/src/views/endpoint.js
+++ src/reactapp/src/src/views/endpoint.js
@@ -1,6 +1,6 @@
 // const endpoint =  "http://10.10.10.10"
 // const endpoint =  "http://localhost:8080"
-// const endpoint =  "https://api.trunglt.info"
-const endpoint =  ""
+const endpoint =  "https://api.trunglt.info"
+// const endpoint =  ""
 
 export default endpoint
